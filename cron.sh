b8ff90cf6609c7737e0e4102fbe716ca5ee29749
Phil Goo Kang <philgookang@gmail.com>
2018-03-26 16:36:41 +0900

    run php script in another sub processs

--- cron.sh
+++ cron.sh
@@ -2,35 +2,35 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 SLEEP=5
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 
 sleep $SLEEP
-php /var/www/philgookang/afreecatv/cron.php
+php /var/www/philgookang/afreecatv/cron.php &
 

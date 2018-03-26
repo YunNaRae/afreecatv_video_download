41bd27ec3338ea8b4a9aaf5f67a0cd04033a413c
Phil Goo Kang <philgookang@gmail.com>
2018-03-26 15:50:38 +0900

    added cron call shell script

---
+++ cron.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+SLEEP=5
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+
+sleep $SLEEP
+php /var/www/philgookang/afreecatv/cron.php
+

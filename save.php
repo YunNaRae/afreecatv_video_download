562f015fb7cdebd031ad17e39ce6424bf412f6d3
Phil Goo Kang <philgookang@gmail.com>
2018-03-26 13:22:35 +0900

    added duplcate check

--- save.php
+++ save.php
@@ -10,15 +10,47 @@
     $xml = simplexml_load_string($_POST['content'], "SimpleXMLElement", LIBXML_NOCDATA);
     $json = json_encode($xml);
     $array = json_decode($json, TRUE);
+    $link = explode('/', $array['share']['link']);
+    $video_id = $link[(count($link) - 1)];
     $title = $array['track']['title'];
     $image_url = $array['track']['titleImage'];
     $video_list = $array['track']['video'][1]['file'];
 
     foreach($video_list as $video) {
-        create($image_url, $title, $video);
+        create($video_id, $image_url, $title, $video);
     }
 
-    function create($screen_url, $title, $url) {
+    function check($video_id) {
+
+        $status = 1;
+
+        $postman = Postman::init();
+        $list = $postman->returnDataList("
+                SELECT
+                    `idx`
+                FROM
+                    `playlist`
+                WHERE
+                    `video_id`=?
+                AND
+                    `status`=?
+            ",
+            array('ii', &$video_id, &$status)
+        );
+
+        if (count($list) > 0)  {
+            return true;
+        }
+
+        return false;
+    }
+
+    function create($video_id, $screen_url, $title, $url) {
+
+        if (check($video_id)) {
+            return;
+        }
+
         $preview    = file_get_contents($screen_url);
         $processed  = 0;
         $status     = 1;
@@ -27,11 +59,11 @@
         $postman->execute("
                 INSERT INTO
                     `playlist`
-                    (`title`, `preview`, `url`, `processed`, `filename`, `created_date_time`, `status`)
+                    (`video_id`, `title`, `preview`, `url`, `processed`, `filename`, `created_date_time`, `status`)
                 VALUES
-                    ( ?, ?, ?, ?, ?, NOW(), ?)
+                    ( ?, ?, ?, ?, ?, ?, NOW(), ?)
             ",
-            array('sssisi', &$title, &$preview, &$url, &$processed, &$filename, &$status)
+            array('isssisi', &$video_id, &$title, &$preview, &$url, &$processed, &$filename, &$status)
         );
     }
 

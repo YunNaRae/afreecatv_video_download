5ce1883e124aec0f1306fb8f9814bfb4a3216a85
Phil Goo Kang <philgookang@gmail.com>
2018-03-26 15:50:22 +0900

    change representation page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
     $list = $postman->returnDataList('
             SELECT
-                `title`,`preview`,`url`,`processed`
+                `title`,`preview`,`filename`,`processed`,`video_id`,`filesize`
             FROM
                 `playlist`
             WHERE
@@ -16,6 +16,34 @@
             ORDER BY
                 `idx` DESC
         ', array('i', &$status));
+
+    $complete_list = array();
+    $video_id = $list[0]->video_id;
+    $tmp = array();
+    foreach($list as $item) {
+        if ($item->video_id != $video_id) {
+            $video_id = $item->video_id;
+            array_push($complete_list, $tmp);
+            $tmp = array();
+        }
+        array_push($tmp, $item);
+    }
+    array_push($complete_list, $tmp);
+
+
+    function formatBytes($bytes, $precision = 2) {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        // Uncomment one of the following alternatives
+        // $bytes /= pow(1024, $pow);
+        // $bytes /= (1 << (10 * $pow));
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
 ?><!doctype html>
 <html>
     <head>
@@ -28,6 +56,12 @@
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+        <style>
+            .prev {
+                background-position: center center; background-repeat: no-repeat; background-size: cover; width: 160px; height: 160px;
+            }
+        </style>
 
     </head>
     <body class="bg-light">
@@ -61,31 +95,38 @@
                     <h4 class="mb-3">Video List</h4>
                     <div class="card">
                         <div class="table-responsive">
-                            <table class="table table-striped table-sm">
+                            <table class="table table-sm">
                                 <thead>
                                     <tr>
                                         <th></th>
                                         <th class="text-center">Title</th>
-                                        <th style="width: 100px;" class="text-center">Status</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php foreach($list as $item) { ?>
+                                    <?php foreach($complete_list as $list) { $item = $list[0]; ?>
                                         <tr>
-                                            <td class="text-center" style="background-image: url(data:image/jpeg;base64,<?php echo base64_encode($item->preview); ?>); background-position: center center; background-repeat: no-repeat; background-size: cover; width: 160px; height: 160px;"></td>
+                                            <td rowspan="2" class="text-center" style="width: 160px; padding: 0px; margin: 0px; font-size: 0px;">
+                                                <div class="prev" style="background-image: url(data:image/jpeg;base64,<?php echo base64_encode($item->preview); ?>);"></div>
+                                            </td>
                                             <td class="align-middle"><?php echo $item->title; ?></td>
-                                            <td class="align-middle text-center">
-                                                <?php if ($item->processed == 0) { ?>
-                                                    대기중
-                                                <?php } else if ($item->processed == 1) { ?>
-                                                    다운로드중
-                                                <?php } else { ?>
-                                                    <a href="#">
-                                                        <div class="btn btn-success">
-                                                            Download
-                                                        </div>
-                                                    </a>
-                                                <?php } ?>
+                                        </tr>
+                                        <tr>
+                                            <td class="align-middle">
+                                                <ol>
+                                                    <?php foreach($list as $item) { ?>
+                                                        <li>
+                                                            <?php if ($item->processed == 0) { ?>
+                                                                대기중
+                                                            <?php } else if ($item->processed == 1) { ?>
+                                                                다운로드중
+                                                            <?php } else { ?>
+                                                                <a href="http://afreecatv.cdn.philgookang.com/afreecatv/<?php echo $item->filename; ?>.mp4">
+                                                                    다운로드 (<?php echo formatBytes($item->filesize); ?>)
+                                                                </a>
+                                                            <?php } ?>
+                                                        </li>
+                                                    <?php } ?>
+                                                </ol>
                                             </td>
                                         </tr>
                                     <?php } ?>

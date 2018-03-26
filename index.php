264524c4cc95cbac932facff63e2fd995f9a224e
Phil Goo Kang <philgookang@gmail.com>
2018-03-26 18:17:10 +0900

    changed page title

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Download</title>
+        <title>아프리카TV 다시 보기 영상 다운로드</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
